259c2b4dee695c247bd7a6640b5cdc2218db7b54
Rachel Queen <rachel.queen@newcastle.ac.uk>
2020-01-07 12:17:47 +0000

    add documentation for sum squares function

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,3 +1,14 @@
+#' Sum Squares Function
+#'
+#' @param  v is a vector of numbers to use to compute sum squares
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' height <- rnorm(100, mean = 10, sd = 0.8)
+#' sum_squares(height)
+
 sum_squares <- function(v) {
   sum((v - mean(v))^2)
 }
