62bea1cb51c707acc267f800e1473dcf7ed70ed9
Rachel Queen <rachel.queen@newcastle.ac.uk>
2020-01-07 13:23:15 +0000

    correct standard error function

--- R/standard_error.R
+++ R/standard_error.R
@@ -1,4 +1,15 @@
-standard_squares <- function(v){
+#' Standard Error Fundtion
+#'
+#' @param v a numeric vector
+#'
+#' @return standard error
+#' @export
+#'
+#' @examples standard_error()
 
-
+standard_error <- function(v){
+  s <- sum(v)
+  n <- length(v)
+  se <- s / sqrt(n)
+  return(se)
 }
